f50a0a7ab85ecf22a29cead9a0381a6c1b788cca
Soliprem <franci.solidoro@gmail.com>
2026-01-16 17:28:19 +0100

    clean: graphene import was unnecessary

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use async_channel::Sender;
 use gdk_pixbuf::Pixbuf;
 use gtk4::{
-    gdk, glib, graphene, prelude::*, Application, ApplicationWindow, EventControllerKey, FlowBox,
+    gdk, glib, prelude::*, Application, ApplicationWindow, EventControllerKey, FlowBox,
     FlowBoxChild, GestureClick, Label, Overlay, Picture, PropagationPhase, ScrolledWindow,
 };
 use rayon::prelude::*;
